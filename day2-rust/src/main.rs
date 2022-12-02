5996dff243a54935289c6be8d4bfe6f63ad395d5
Lee Smith <lee.smith4@itv.com>
2022-12-02 12:55:39 +0000

    Second half day 2

--- day2-rust/src/main.rs
+++ day2-rust/src/main.rs
@@ -15,10 +15,8 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         match (line_input.get(0), line_input.get(1)) {
             (Some(elf_play_string), Some(you_play_string)) => {
                 let elf_play = Play::from_str(elf_play_string)?;
-                let you_play = Play::from_str(you_play_string)?;
 
-                total_points = total_points + you_play.result(elf_play);
-                total_points = total_points + you_play.value();
+                total_points = total_points + elf_play.result(you_play_string)?;
             }
             _ => ()
         }
@@ -34,25 +32,25 @@ enum Play {
 }
 
 #[derive(Debug, Clone)]
-struct EmptyVec;
+struct GeneralError;
 
-impl fmt::Display for EmptyVec {
+impl fmt::Display for GeneralError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "invalid first item to double")
+        write!(f, "General lazy error")
     }
 }
 
-impl error::Error for EmptyVec {}
+impl error::Error for GeneralError {}
 
 impl FromStr for Play {
-    type Err = EmptyVec;
+    type Err = GeneralError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "A" | "X" => Ok(Play::Rock),
-            "B" | "Y" => Ok(Play::Paper),
-            "C" | "Z" => Ok(Play::Scissors),
-            _ => Err(EmptyVec)
+            "A" => Ok(Play::Rock),
+            "B" => Ok(Play::Paper),
+            "C" => Ok(Play::Scissors),
+            _ => Err(GeneralError)
         }
     }
 }
@@ -70,17 +68,18 @@ impl Play {
     const DRAW_POINTS: u32 = 3;
     const WIN_POINTS: u32 = 6;
 
-    fn result(&self, opponent_play: Play) -> u32 {
-        match (self, opponent_play) {
-            (Play::Rock, Play::Rock) => Play::DRAW_POINTS,
-            (Play::Rock, Play::Paper) => Play::LOSE_POINTS,
-            (Play::Rock, Play::Scissors) => Play::WIN_POINTS,
-            (Play::Paper, Play::Rock) => Play::WIN_POINTS,
-            (Play::Paper, Play::Paper) => Play::DRAW_POINTS,
-            (Play::Paper, Play::Scissors) => Play::LOSE_POINTS,
-            (Play::Scissors, Play::Rock) => Play::LOSE_POINTS,
-            (Play::Scissors, Play::Paper) => Play::WIN_POINTS,
-            (Play::Scissors, Play::Scissors) => Play::DRAW_POINTS
+    fn result(&self, your_move: &str) -> Result<u32, Box<dyn error::Error>> {
+        match (self, your_move) {
+            (Play::Rock, "X") => Ok(Play::LOSE_POINTS + Play::Scissors.value()),
+            (Play::Rock, "Y") => Ok(Play::DRAW_POINTS + Play::Rock.value()),
+            (Play::Rock, "Z") => Ok(Play::WIN_POINTS + Play::Paper.value()),
+            (Play::Paper, "X") => Ok(Play::LOSE_POINTS + Play::Rock.value()),
+            (Play::Paper, "Y") => Ok(Play::DRAW_POINTS + Play::Paper.value()),
+            (Play::Paper, "Z") => Ok(Play::WIN_POINTS + Play::Scissors.value()),
+            (Play::Scissors, "X") => Ok(Play::LOSE_POINTS + Play::Paper.value()),
+            (Play::Scissors, "Y") => Ok(Play::DRAW_POINTS + Play::Scissors.value()),
+            (Play::Scissors, "Z") => Ok(Play::WIN_POINTS + Play::Rock.value()),
+            _ => Err(Box::new(GeneralError))
         }
     }
 }
