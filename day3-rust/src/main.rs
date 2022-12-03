5e756dde8633f75e92b355c163340f1010c0b1c4
Lee Smith <lee.smith4@itv.com>
2022-12-03 20:32:15 +0000

    Day 3 part 2

--- day3-rust/src/main.rs
+++ day3-rust/src/main.rs
@@ -1,26 +1,36 @@
+use itertools::Itertools;
+
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
     let input = include_str!("input.txt");
     let mut sum = 0;
-    for line in input.lines() {
-        let (left, right) = line.split_at(line.len() / 2);
-        let mut left_chars = left.chars().into_iter();
+    let mut batched = input.lines().chunks(3);
+
+    for chunk in batched.into_iter() {
+        let chunk_vec = chunk.collect::<Vec<_>>();
+        let mut g1_chars = chunk_vec[0].chars();
         loop {
-            if let Some(left_char) = left_chars.next() {
-                if right.chars().collect::<Vec<_>>().contains(&left_char) {
-                    if left_char.is_uppercase() {
-                        println!("Got upper {} from {}", left_char, line);
-                        sum = sum + (left_char as u32) - 38;
-                    } else {
-                        println!("Got lower {} from {}", left_char, line);
-                        sum = sum + (left_char as u32) - 96;
-                    }
+            if let Some(g1_char) = g1_chars.next() {
+                if chunk_vec[1].chars().collect::<Vec<_>>().contains(&g1_char) && chunk_vec[2].chars().collect::<Vec<_>>().contains(&g1_char) {
+                    sum = sum + get_position(chunk_vec[0], g1_char);
                     break;
+                } else {
+                    println!("None in {}, {}, {}", chunk_vec[0], chunk_vec[1], chunk_vec[2]);
                 }
             }
         }
     }
 
     Ok(println!("{sum}"))
+}
+
+fn get_position(line: &str, left_char: char) -> u32 {
+    if left_char.is_uppercase() {
+        println!("Got upper {} from {}", left_char, line);
+        (left_char as u32) - 38
+    } else {
+        println!("Got lower {} from {}", left_char, line);
+        (left_char as u32) - 96
+    }
 }
