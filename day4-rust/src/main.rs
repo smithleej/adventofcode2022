4137d73b6864db551f25b6f40617478417ff2e20
Lee Smith <lee.smith4@itv.com>
2022-12-04 17:26:08 +0000

    Day 4 part 2

--- day4-rust/src/main.rs
+++ day4-rust/src/main.rs
@@ -1,5 +1,3 @@
-// use color_eyre::eyre::eyre;
-
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
@@ -9,11 +7,13 @@ fn main() -> color_eyre::Result<()> {
         let (first_elf, second_elf) = (line[0], line[1]);
         let first_elf_sections = first_elf.split("-").map(|string|string.parse::<u32>().unwrap()).collect::<Vec<_>>();
         let second_elf_sections = second_elf.split("-").map(|string|string.parse::<u32>().unwrap()).collect::<Vec<_>>();
-        if first_elf_sections[0] <= second_elf_sections[0] && first_elf_sections[1] >= second_elf_sections[1] {
-            println!("Second {:?} contained in {:?}", second_elf_sections, first_elf_sections);
+        if first_elf_sections[0] >= second_elf_sections[0] && first_elf_sections[0] <= second_elf_sections[1] {
             1
-        } else if second_elf_sections[0] <= first_elf_sections[0] && second_elf_sections[1] >= first_elf_sections[1] {
-            println!("First {:?} contained in {:?}", first_elf_sections, second_elf_sections);
+        } else if first_elf_sections[1] >= second_elf_sections[0] && first_elf_sections[1] <= second_elf_sections[1]  {
+            1
+        } else if second_elf_sections[0] >= first_elf_sections[0] && second_elf_sections[0] <= first_elf_sections[1] {
+            1
+        } else if second_elf_sections[1] >= first_elf_sections[0] && second_elf_sections[1] <= first_elf_sections[1]  {
             1
         } else { 0 }
     }).sum();
